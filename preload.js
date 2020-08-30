ae2bd46a236eea636b893e60735ffc49e9d4b0df
onesword0618 <ao.akua.leo@gmail.com>
2020-08-31 08:40:16 +0900

    Update: APIのリンクを付与した

--- preload.js
+++ preload.js
@@ -5,8 +5,4 @@ window.addEventListener('DOMContentLoaded', () => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
 })
